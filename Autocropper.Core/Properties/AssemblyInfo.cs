2b76ff4f42baedaa2067d370daf3b0a0739005f3
Zachary Kniebel <zkniebel@delphicdigital.com>
2015-09-03 11:51:52 -0400

    Initial commit:
    - Based on Sitecore 8.0 rev. 150621
    - References folder included in Repository includes ALL dll files included with Sitecore 8.0 rev. 150621
    - Most basic Sitecore files excluded so as to not pollute repository
    - Commands.config included has not been modified from Sitecore default
    - Includes unimplemented Autocropper.Core.Shell.Framework.Commands.InsertAutocroppedImage class

---
+++ Autocropper.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Autocropper.Core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Autocropper.Core")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b194b71f-5704-476c-82e1-2de54f1da29f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Autocropper.Core/Shell/Framework/Commands/InsertAutocroppedImage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Shell.Framework.Commands;
+
+namespace Autocropper.Core.Shell.Framework.Commands
+{
+    public class InsertAutocroppedImage : Sitecore.Shell.Framework.Commands.Command
+    {
+        public override void Execute(CommandContext context)
+        {
+            //TODO: Implement
+            throw new NotImplementedException("Implement InsertAutocroppedImage.Execute");
+        }
+    }
+}
